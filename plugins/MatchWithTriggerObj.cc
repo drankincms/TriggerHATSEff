5405fbf3e56bdd0c69ed4d128062744b94fd2278
Dylan <drankin@bu.edu>
2014-08-12 09:28:48 -0500

    testing git

--- plugins/MatchWithTriggerObj.cc
+++ plugins/MatchWithTriggerObj.cc
@@ -30,12 +30,16 @@ MatchWithTriggerObj::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
     using namespace edm;
     using namespace std;
 
+//testing git
+
     edm::Handle<GenEventInfoProduct> genEvent;
     iEvent.getByLabel("generator", genEvent);
 
     edm::Handle<reco::GenParticleCollection> genParticles;
     iEvent.getByLabel( "genParticles", genParticles );
     
+    edm::Handle<reco::GenJetCollection> ak5GenJets;
+    iEvent.getByLabel( "ak5GenJets", ak5GenJets );
     
     bool changedConfig = false;
     if (!hltConfig.init(iEvent.getRun(), iSetup, "HLT", changedConfig)) {
@@ -44,11 +48,13 @@ MatchWithTriggerObj::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
     }
     if (changedConfig){
         std::cout << "the curent menu is " << hltConfig.tableName() << std::endl;
-        triggerBit = -1;
+        triggerBitMu = -1;
+        triggerBitMu3Jets = -1;
         for (size_t j = 0; j < hltConfig.triggerNames().size(); j++) {
-            if (TString(hltConfig.triggerNames()[j]).Contains("HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL")) triggerBit = j;
+            if (TString(hltConfig.triggerNames()[j]).Contains("HLT_IsoMu17_eta2p1_TriCentralPFNoPUJet30_v")) triggerBitMu3Jets = j;
+            if (TString(hltConfig.triggerNames()[j]).Contains("HLT_IsoMu24_v")) triggerBitMu = j;
         }
-        if (triggerBit == -1) cout << "HLT path not found" << endl;
+        if (triggerBitMu == -1 || triggerBitMu3Jets) cout << "HLT path not found" << endl;
        
     }
     
@@ -59,23 +65,36 @@ MatchWithTriggerObj::analyze(const edm::Event& iEvent, const edm::EventSetup& iS
     
     
 
-    
     int theNbOfGen = genParticles->size();
     for (int i=0 ; i < theNbOfGen; i++){
         const reco::GenParticle & genMuon = (*genParticles)[i];
-        if ((fabs(genMuon.pdgId())==13)&&(genMuon.status()==1)&&(hasWasMother(genMuon))){
+        if ((fabs(genMuon.pdgId())==13)&&(genMuon.status()==1)){
             if ((fabs(genMuon.eta())>2.4)) continue;
-            for (int j=0 ; j < theNbOfGen; j++){
-                if (i==j) continue;
-                const reco::GenParticle & genElectron = (*genParticles)[j];
-                if ((fabs(genElectron.pdgId())==11)&&(genElectron.status()==1)&&(hasWasMother(genElectron))){
-                    if ((fabs(genElectron.eta())>2.5)) continue;//eta acceptance cut
-                    histos2D_[ "ptDenom" ]->Fill(genMuon.pt(), genElectron.pt());
-                    if (triggerResults->accept(triggerBit)) histos2D_[ "ptPassing" ]->Fill(genMuon.pt(), genElectron.pt(),1);
-                    else  histos2D_[ "ptPassing" ]->Fill(genMuon.pt(), genElectron.pt(),0);
-                }
-            }
+            histos1D_[ "ptDenom" ]->Fill(genMuon.pt());
+            if (triggerResults->accept(triggerBitMu)) histos1D_[ "ptPassing" ]->Fill(genMuon.pt(),1);
+            else histos1D_[ "ptPassing" ]->Fill(genMuon.pt(),0);
             
+        }
+    }
+    
+    int theNbofGenJets = ak5GenJets->size();
+    double leadPT=0;
+    double subleadPT;
+    for (int i=0 ; i < theNbOfGen; i++){
+        const reco::GenParticle & genMuon = (*genParticles)[i];
+        if ((fabs(genMuon.pdgId())==13)&&(genMuon.status()==1)&&theNbofGenJets>=3){
+            if ((fabs(genMuon.eta())>2.4)) continue;
+            for (int j=0 ; j < theNbofGenJets ; j++){
+                const reco::GenJet & genJet = (*ak5GenJets)[j];
+                if ((genJet.pt())>leadPT) {
+                    subleadPT=leadPT;
+                    leadPT=genJet.pt();
+                }
+                else if ((genJet.pt())>subleadPT) subleadPT=genJet.pt();
+            }
+            histos2D_[ "ptDenom" ]->Fill(genMuon.pt(),leadPT);
+            if (triggerResults->accept(triggerBitMu3Jets)) histos2D_[ "ptPassing" ]->Fill(genMuon.pt(),leadPT,1);
+            else histos2D_[ "ptPassing" ]->Fill(genMuon.pt(),leadPT,0);
         }
     }
 
@@ -87,18 +106,26 @@ void
 MatchWithTriggerObj::beginJob()
 {
     edm::Service< TFileService > fileService;
-    histos2D_[ "ptDenom" ] = fileService->make< TH2D >( "ptDenom", "nb of muon-electron pair", 10, 0., 100, 10, 0., 100. );
+    histos1D_[ "ptDenom" ] = fileService->make< TH1D >( "ptDenom", "nb of muon pair", 10, 0., 100);
+    histos1D_[ "ptDenom" ]->SetXTitle( "muon p_{T} (GeV)" );
+    
+    histos1D_[ "ptPassing" ] = fileService->make< TH1D >( "ptPassing", "nb of muon passing path", 10, 0., 100);
+    histos1D_[ "ptPassing" ]->SetXTitle( "muon p_{T} (GeV)" );
+    
+    histos1D_[ "pt1Defficiency" ] = fileService->make< TH1D >( "pt1Defficiency", "efficiency", 10, 0., 100);
+    histos1D_[ "pt1Defficiency" ]->SetXTitle( "muon p_{T} (GeV)" );
+    
+    histos2D_[ "ptDenom" ] = fileService->make< TH2D >( "ptDenom", "nb of muon+3jets", 10, 0., 100, 10, 0., 100);
     histos2D_[ "ptDenom" ]->SetXTitle( "muon p_{T} (GeV)" );
-    histos2D_[ "ptDenom" ]->SetYTitle( "electron p_{T} (GeV)" );
+    histos2D_[ "ptDenom" ]->SetYTitle( "lead jet p_{T} (GeV)" );
     
-    histos2D_[ "ptPassing" ] = fileService->make< TH2D >( "ptPassing", "nb of muon-electron pair passing path", 10, 0., 100, 10, 0., 100. );
+    histos2D_[ "ptPassing" ] = fileService->make< TH2D >( "ptPassing", "nb of muon+3jets passing path", 10, 0., 100, 10, 0., 100);
     histos2D_[ "ptPassing" ]->SetXTitle( "muon p_{T} (GeV)" );
-    histos2D_[ "ptPassing" ]->SetYTitle( "electron p_{T} (GeV)" );
+    histos2D_[ "ptPassing" ]->SetYTitle( "lead jet p_{T} (GeV)" );
     
-    histos2D_[ "pt2Defficiency" ] = fileService->make< TH2D >( "pt2Defficiency", "efficiency", 10, 0., 100, 10, 0., 100. );
+    histos2D_[ "pt2Defficiency" ] = fileService->make< TH2D >( "pt2Defficiency", "efficiency", 10, 0., 100, 10, 0., 100);
     histos2D_[ "pt2Defficiency" ]->SetXTitle( "muon p_{T} (GeV)" );
-    histos2D_[ "pt2Defficiency" ]->SetYTitle( "electron p_{T} (GeV)" );
-    
+    histos2D_[ "pt2Defficiency" ]->SetYTitle( "lead jet p_{T} (GeV)" );
 
 }
 
@@ -109,27 +136,11 @@ MatchWithTriggerObj::endJob()
 
     histos2D_[ "pt2Defficiency" ]->Sumw2();
     histos2D_[ "pt2Defficiency" ]->Divide(histos2D_[ "ptPassing" ],histos2D_[ "ptDenom" ],1,1);
+
+    histos1D_[ "pt1Defficiency" ]->Sumw2();
+    histos1D_[ "pt1Defficiency" ]->Divide(histos1D_[ "ptPassing" ],histos1D_[ "ptDenom" ]);
     
   }
-
-bool
-MatchWithTriggerObj::hasWasMother(const reco::GenParticle  p)
-{
-    bool foundW = false;
-    if (p.numberOfMothers()==0) return foundW;
-    const reco::Candidate  *part = (p.mother());
-    // loop on the mother particles to check if is has a W has mother
-    while ((part->numberOfMothers()>0)) {
-        const reco::Candidate  *MomPart =part->mother();
-        if (fabs(MomPart->pdgId())==24){
-            foundW = true;
-            break;
-        }
-        part = MomPart;
-    }
-    return foundW;
-}
-
 
 
 // ------------ method called when starting to processes a run  ------------
